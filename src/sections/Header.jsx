1e8a412a5cfef261d4f95867c6d1eb8dedd9db05
NataliaReiis <nataliareis@gmail.com>
2023-09-01 17:00:16 -0300

    inclui: header

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,5 @@
 import Logo from '../assets/img/logomarca1.png';
-
+import Home from  '../assets/img/img-home.png'
 export default function Header() {
     return(
     <>
@@ -15,6 +15,17 @@ export default function Header() {
             </nav>
             <button>Contato</button>
        </header>
+
+       <section className='container-header'>
+            <div className='content-txt'>
+                <h1>KS Solutions</h1>
+                <p>Conectando pessoas e soluções através do uso consciente e efetivo das tecnologias disponíveis para aprimorar processos e rotinas.</p>
+                <button>Ver mais</button>
+            </div>
+            <div className='content-img'>
+                <img src={Home} alt="" />
+            </div>
+       </section>
     
     </>
     )
